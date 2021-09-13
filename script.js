1bc30334efc99b9849fe29bd320f9d2d25f60938
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-09-14 00:06:00 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const elTitle = document.querySelector('#title');
+const elInput = document.querySelector('#input');
+const elRain = document.querySelector('#rain');
+const elzal = document.querySelector('#zal');
+
+elInput.addEventListener('keyup', function(e){
+    e.preventDefault()
+    
+    if(!isNaN(elInput.value)){
+        if(elInput.value > 5 && elInput.value < 30){
+            elTitle.textContent = 'HA'
+        }else{
+            elTitle.textContent = 'YO\'Q'
+        }
+    }else{
+        elTitle.textContent = 'Raqam kiriting'
+    }
+})
+
+
+elRain.addEventListener('click', function(){
+    if(elRain.checked){
+        elTitle.textContent = 'YO\'Q'
+    }else{
+        elTitle.textContent = 'HA'
+    }
+})
+
+elzal.addEventListener('click', function(){
+    if(elRain.checked && elzal.checked){
+        elTitle.textContent = 'HA'
+    }else{
+        if(elzal.checked){
+            elTitle.textContent = 'HA'
+        }else{
+            if(elInput.value < 5 && elzal.checked){
+                elTitle.textContent = 'YO\'Q'
+            }else{
+                elTitle.textContent = 'HA'
+            }
+        }
+    }
+})
